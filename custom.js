d85859cf79a36afb85d58652756f9255613ab9e3
hypermoobs <156623463+Hypermoobs@users.noreply.github.com>
2024-05-21 23:11:55 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -22,11 +22,11 @@ function sendHourlyMessage(api, message) {
 
 function init(api) {
 
-    const message = "Hey 👋 Wanna Create Bot like this you can fork this project here https://github.com/hardasf/OctoBotRemake and start exploring 🥰 any suggestions feedback don't hesitate to contact us❤️";
+    const message = "Hey 👋 pogi ko no?";
     
     /* using fs 
     const message  = {
-        body: `WELCOME TO YETANOTHERFBBOT`,
+        body: `WELCOME TO Tantan's world`,
         attachment: fs.createReadStream('cache/logo1.png')
       };
       */
